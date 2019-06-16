09d864da4953005111621ba27732cbbe3cacc352
neerulaxnesh <44649968+neerulaxnesh@users.noreply.github.com>
2019-06-16 07:31:38 +0545

    Add files via upload
    result may not display

---
+++ palindromerecursion.py
@@ -0,0 +1,17 @@
+def palindrome(x) :
+    global rev
+    rem=0
+    rev=0
+    while (x>0) :
+        rem = x%10
+        rev = (rev*10) + rem
+       
+    palindrome(x//10)
+    return  rev
+
+num=int(input('enter a integer'))
+check=palindrome(num)
+if (check==num) :
+        print(num,'is palindrome')
+else :
+        print(num,'is not palindrome')
